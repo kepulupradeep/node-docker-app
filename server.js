46f12587a779198ff53d854ae8ed48fe8aab850b
root <root@ip-172-31-35-69.ec2.internal>
2026-08-03 08:15:31 +0000

    content updated

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Node.js application deployed automatically to EC2!");
+  res.send("Node.js application deployed automatically to EC2!!");
 });
 
 app.get("/health", (req, res) => {
